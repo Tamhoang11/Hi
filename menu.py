0c41962ae8c0fd5a9841c0ea9e38d634d508bda9
Tamhoang11 <162526329+Tamhoang11@users.noreply.github.com>
2024-09-21 00:06:24 +0700

    Ha
    Em

---
+++ menu.py
@@ -0,0 +1,40 @@
+import os
+from zlapi.models import Message, MultiMsgStyle, MessageStyle
+import importlib
+
+def get_all_mitaizl():
+    mitaizl = {}
+
+    for module_name in os.listdir('modules'):
+        if module_name.endswith('.py') and module_name != '__init__.py':
+            module_path = f'modules.{module_name[:-3]}'
+            module = importlib.import_module(module_path)
+
+            if hasattr(module, 'get_mitaizl'):
+                module_mitaizl = module.get_mitaizl()
+                mitaizl.update(module_mitaizl)
+
+    command_names = list(mitaizl.keys())
+    return command_names
+
+def handle_menu_command(message, message_object, thread_id, thread_type, author_id, client):
+    command_names = get_all_mitaizl()
+
+    total_mitaizl = len(command_names)
+    numbered_mitaizl = [f"{i+1}. {name}" for i, name in enumerate(command_names)]
+    menu_message = f"Tổng số lệnh bot hiện tại có: {total_mitaizl} lệnh \nDưới đây là các lệnh hiện có của bot:\n" + "\n".join(numbered_mitaizl)
+
+    msg_length = len(menu_message)
+    style = MultiMsgStyle([
+        MessageStyle(offset=0, length=msg_length, style="color", color="cdd6f4", auto_format=False),
+        MessageStyle(offset=0, length=msg_length, style="font", size="14", auto_format=False),
+        MessageStyle(offset=0, length=msg_length, style="bold", auto_format=False)
+    ])
+
+    message_to_send = Message(text=menu_message, style=style)
+    client.replyMessage(message_to_send, message_object, thread_id, thread_type)
+
+def get_mitaizl():
+    return {
+        'menu': handle_menu_command
+    }
